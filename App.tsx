ea0434902878c4945c25a8a20ee69b00cb427634
jeet-parshwa <78416198+jeet-dhandha@users.noreply.github.com>
2022-05-14 18:57:59 +0530

    First Comit

--- App.tsx
+++ App.tsx
@@ -8,37 +8,14 @@
  * @format
  */
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { View, SafeAreaView } from "react-native";
+import { View } from "react-native";
 import "react-native-gesture-handler";
-import Home from "./src/Screens/Home/Home";
-import Launch from "./src/Screens/Launch/Launch";
-import PlaceInfo from "./src/Screens/Placeinfo/PlaceInfo";
-const Stack = createStackNavigator();
+import Home from "./src/Screens/Home";
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Launch"
-            component={Launch}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="PlaceInfo"
-            component={PlaceInfo}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Home />
     </View>
   );
 };
